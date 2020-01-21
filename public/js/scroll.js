906c5ac79811b0a6c46ef864ee74c3e84f5f9636
Pedro Antonio <pedroantonio.aragao@gmail.com>
2020-01-21 17:33:19 -0300

    Beta 1.0.9

--- public/js/scroll.js
+++
@@ -1,8 +0,0 @@
-$('.menu a').click(function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    console.log($(this).attr('href'));
-    $('html, body').animate({
-        scrollTop: $($(this).attr('href')).offset().top
-    },1000);
-});
